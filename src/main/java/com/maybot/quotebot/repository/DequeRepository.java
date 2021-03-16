023ebbc96db31f3868c61e8164ba51a3c2e36829
Mayra <mayra.cademartori@gmail.com>
2021-03-16 19:21:19 -0300

    Fix(Transactional Pop Quote from Deque)

--- src/main/java/com/maybot/quotebot/repository/DequeRepository.java
+++ src/main/java/com/maybot/quotebot/repository/DequeRepository.java
@@ -1,10 +1,14 @@
 package com.maybot.quotebot.repository;
 
 import com.maybot.quotebot.entity.Deque;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,4 +21,9 @@ public interface DequeRepository extends CrudRepository<Deque, Long>  {
 
     @Query("SELECT d FROM Deque d ORDER BY d.priority DESC, d.id")
     Optional<Deque> findPriorityFirst(PageRequest pageable);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Deque d WHERE d.id = :id")
+    void deleteById(@NotNull @Param("id") Long id);
 }
